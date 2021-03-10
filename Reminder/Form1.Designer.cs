22994c07f39c708cd6b0eb5ee2c3e61478ec1f43
AdamTriadic <65728487+AdamTriadic@users.noreply.github.com>
2021-03-10 21:17:00 +0000

    Initial Push

---
+++ Reminder/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace Reminder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SearchTxtBox = new System.Windows.Forms.TextBox();
+            this.SearchBtn = new System.Windows.Forms.Button();
+            this.searchResults = new System.Windows.Forms.ListView();
+            this.TitleColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.YearColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.IMDBColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.TypeColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.PosterColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.resultsTotalLabel = new System.Windows.Forms.Label();
+            this.nextBtn = new System.Windows.Forms.Button();
+            this.backBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // SearchTxtBox
+            // 
+            this.SearchTxtBox.Location = new System.Drawing.Point(12, 12);
+            this.SearchTxtBox.Name = "SearchTxtBox";
+            this.SearchTxtBox.Size = new System.Drawing.Size(277, 20);
+            this.SearchTxtBox.TabIndex = 0;
+            // 
+            // SearchBtn
+            // 
+            this.SearchBtn.Location = new System.Drawing.Point(295, 12);
+            this.SearchBtn.Name = "SearchBtn";
+            this.SearchBtn.Size = new System.Drawing.Size(75, 20);
+            this.SearchBtn.TabIndex = 1;
+            this.SearchBtn.Text = "Search";
+            this.SearchBtn.UseVisualStyleBackColor = true;
+            this.SearchBtn.Click += new System.EventHandler(this.SearchBtn_Click);
+            // 
+            // searchResults
+            // 
+            this.searchResults.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.TitleColumn,
+            this.YearColumn,
+            this.IMDBColumn,
+            this.TypeColumn,
+            this.PosterColumn});
+            this.searchResults.FullRowSelect = true;
+            this.searchResults.HideSelection = false;
+            this.searchResults.Location = new System.Drawing.Point(12, 38);
+            this.searchResults.Name = "searchResults";
+            this.searchResults.Size = new System.Drawing.Size(726, 241);
+            this.searchResults.TabIndex = 2;
+            this.searchResults.UseCompatibleStateImageBehavior = false;
+            this.searchResults.View = System.Windows.Forms.View.Details;
+            this.searchResults.ItemActivate += new System.EventHandler(this.searchResults_ItemActivate);
+            // 
+            // TitleColumn
+            // 
+            this.TitleColumn.Text = "Title";
+            this.TitleColumn.Width = 213;
+            // 
+            // YearColumn
+            // 
+            this.YearColumn.Text = "Year";
+            this.YearColumn.Width = 70;
+            // 
+            // IMDBColumn
+            // 
+            this.IMDBColumn.Text = "IMDb ID";
+            this.IMDBColumn.Width = 100;
+            // 
+            // TypeColumn
+            // 
+            this.TypeColumn.Text = "Type";
+            this.TypeColumn.Width = 103;
+            // 
+            // PosterColumn
+            // 
+            this.PosterColumn.Text = "Poster URL";
+            this.PosterColumn.Width = 216;
+            // 
+            // resultsTotalLabel
+            // 
+            this.resultsTotalLabel.Location = new System.Drawing.Point(376, 11);
+            this.resultsTotalLabel.Name = "resultsTotalLabel";
+            this.resultsTotalLabel.Size = new System.Drawing.Size(200, 23);
+            this.resultsTotalLabel.TabIndex = 3;
+            this.resultsTotalLabel.Text = "1000";
+            this.resultsTotalLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // nextBtn
+            // 
+            this.nextBtn.Location = new System.Drawing.Point(663, 12);
+            this.nextBtn.Name = "nextBtn";
+            this.nextBtn.Size = new System.Drawing.Size(75, 20);
+            this.nextBtn.TabIndex = 4;
+            this.nextBtn.Text = "Next";
+            this.nextBtn.UseVisualStyleBackColor = true;
+            // 
+            // backBtn
+            // 
+            this.backBtn.Enabled = false;
+            this.backBtn.Location = new System.Drawing.Point(582, 12);
+            this.backBtn.Name = "backBtn";
+            this.backBtn.Size = new System.Drawing.Size(75, 20);
+            this.backBtn.TabIndex = 5;
+            this.backBtn.Text = "Back";
+            this.backBtn.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(757, 295);
+            this.Controls.Add(this.backBtn);
+            this.Controls.Add(this.nextBtn);
+            this.Controls.Add(this.resultsTotalLabel);
+            this.Controls.Add(this.searchResults);
+            this.Controls.Add(this.SearchBtn);
+            this.Controls.Add(this.SearchTxtBox);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Reminder";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox SearchTxtBox;
+        private System.Windows.Forms.Button SearchBtn;
+        private System.Windows.Forms.ListView searchResults;
+        private System.Windows.Forms.ColumnHeader TitleColumn;
+        private System.Windows.Forms.ColumnHeader TypeColumn;
+        private System.Windows.Forms.ColumnHeader YearColumn;
+        private System.Windows.Forms.ColumnHeader IMDBColumn;
+        private System.Windows.Forms.ColumnHeader PosterColumn;
+        private System.Windows.Forms.Label resultsTotalLabel;
+        private System.Windows.Forms.Button nextBtn;
+        private System.Windows.Forms.Button backBtn;
+    }
+}
+
---
+++ Reminder/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using System.Text.Json;
+using Newtonsoft.Json;
+
+namespace Reminder
+{
+    public partial class Form1 : Form
+    {
+        public string apiURL = "http://www.omdbapi.com/?apikey=1574810&";
+
+        int currentPageIndex = 1;
+        int totalPages = 1;
+
+        public class SearchResults
+        {
+            public List<Titles> Search { get; set; }
+            public string totalResults { get; set; }
+            public bool Response { get; set; }
+        }
+
+        public class Titles
+        {
+            public string Title { get; set; }
+            public string Year { get; set; }
+            public string imdbID { get; set; }
+            public string Type { get; set; }
+            public string Poster { get; set; }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            ResetSearch();
+        }
+
+        private void ResetSearch()
+        {
+            searchResults.Items.Clear();
+            resultsTotalLabel.Text = null;
+        }
+
+        public string CreateURL(bool isSearch, string titleRequested, int pageNumber)
+        {
+            string url = apiURL;
+
+            if (isSearch)
+            {
+                url += "s=" + titleRequested + "&page=" + pageNumber;
+            }
+            else
+            {
+                url += "i=" + titleRequested;
+            }
+
+            url += "&r=json";
+
+            return url;
+        }
+
+        private void searchResults_ItemActivate(object sender, EventArgs e)
+        {
+            HttpClient client = new HttpClient();
+            var json = client.GetStringAsync(CreateURL(false, searchResults.SelectedItems[0].SubItems[2].Text, 1));
+            var results = JsonConvert.DeserializeObject(json.Result);
+            MessageBox.Show(results.ToString());
+        }
+
+        private void SearchBtn_Click(object sender, EventArgs e)
+        {
+            if (SearchTxtBox.Text != null)
+            {
+
+                HttpClient client = new HttpClient();
+                var json = client.GetStringAsync(CreateURL(true, SearchTxtBox.Text, 1));
+                var results = JsonConvert.DeserializeObject<SearchResults>(json.Result);
+
+                totalPages = Convert.ToInt32(results.totalResults) / 10;
+                resultsTotalLabel.Text = currentPageIndex + "/" + totalPages + " Pages";
+
+                foreach (Titles result in results.Search)
+                {
+                    ListViewItem item = new ListViewItem(result.Title);
+                    item.SubItems.Add(result.Year);
+                    item.SubItems.Add(result.imdbID);
+                    item.SubItems.Add(result.Type);
+                    item.SubItems.Add(result.Poster);
+                    searchResults.Items.Add(item);
+                }
+            }
+        }
+    }
+}
